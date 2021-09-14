605f0a5daeea1abc9a30ef8070dc64e698f7ca74
Carlos Efren Fernandez Abad <54074960+ingcarlosefren@users.noreply.github.com>
2021-09-14 10:49:03 +0200

    Update TestConexion.php
    remove pass

--- TestConexion.php
+++ TestConexion.php
@@ -2,14 +2,14 @@
 require 'vendor/autoload.php';
 
     try{
-        $connection = new MongoDB\Client('mongodb://cefa-user:7869097@150.136.170.184:27017/?authSource=cefa-data');
+        $connection = new MongoDB\Client('mongodb://cefa-user:******@*********:27017/?authSource=cefa-data');
         if($connection){
             $database=$connection ->BlogDB;
             $collection = $database->posts;
             $result = $collection->findOne();
             echo 'Conexion Exitosa y activa...';
             //busqueda especifica
-            $collection2 = (new MongoDB\Client('mongodb://cefa-user:7869097@150.136.170.184:27017/?authSource=cefa-data'))->BlogDB->posts;
+            $collection2 = (new MongoDB\Client('mongodb://cefa-user:*********@**********:27017/?authSource=cefa-data'))->BlogDB->posts;
             $distinct = $collection2->distinct('comments.author');
 
             foreach($distinct as $document){
