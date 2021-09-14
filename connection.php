830ce4a10e2784af762da3530c89ca96f2d5d832
Carlos Efren Fernandez Abad <54074960+ingcarlosefren@users.noreply.github.com>
2021-09-14 11:13:13 +0200

    Update connection.php
    Actualización conexión a base de datos oracle

--- connection.php
+++ connection.php
@@ -3,13 +3,13 @@ require 'vendor/autoload.php';
 
     try{
         // Setup MongoDB connection
-        $connection = new MongoDB\Client('mongodb://mongo-admin:mongo@127.0.0.1:27017');
+        $connection = new MongoDB\Client('mongodb://cefa-user:7869097@150.136.170.184:27017/?authSource=cefa-data');
 
         /**********
         ** YOUR CODE HERE:
         Assign to $connection the connection to MongoDB
         **********/
-        $database=$connection ->blog;
+        $database=$connection ->BlogDB;
 
         // Select the "posts" collection in the database "blog"
         $collection = $database->posts;
